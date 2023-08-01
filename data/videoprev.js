aab015f55b7016121923156e89196fcd2cde0ab4
Falalatina <karolina40.gorska17@gmail.com>
2023-08-01 12:45:10 +0200

    some my changes

---
+++ data/videoprev.js
@@ -0,0 +1,11 @@
+videoprew = [
+  {
+    image: 'images/thumbnail-1.webp',
+    title: 'Talking Tech and AI with Google CEO Sundar Pichai!',
+    author: 'Marques Brownlee',
+    stats:{
+      views: 3.4,
+      data: 6
+    }
+  }
+]
