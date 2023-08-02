88abe7c6ce94f9408a9247d28d6692635bf52f3e
Falalatina <karolina40.gorska17@gmail.com>
2023-08-02 13:26:37 +0200

    lesson 9 cd

--- data/addVidPrwV.js
+++ data/addVidPrwV.js
@@ -1,2 +1,15 @@
 video = [];
 
+let renderHTML = '';
+
+videoprev.forEach(element => {
+  renderHTML = renderHTML + `
+    <img class="tumbnail" src="${element.image}">  
+    <p class="video-title">${element.title}</p>
+    <p class="video-author">${element.author}</p>
+    <p class="video-stats">${element.stats.views}M views &#183; ${element.stats.data} months ago</p>
+  `;
+});
+
+document.querySelector('.video-preview')
+  .innerHTML = renderHTML;
